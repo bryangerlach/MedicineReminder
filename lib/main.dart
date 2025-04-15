749aa8a9febc2d8355531241b182efc96918f192
Bryan Gerlach <bryan.r.gerlach@gmail.com>
2025-04-14 21:22:18 -0500

    update some stuff, get it working

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env"); // Load the .env file
+  await dotenv.load(fileName: "dotenv"); // Load the .env file
   if (!kIsWeb) {
     notificationInit();
   }
